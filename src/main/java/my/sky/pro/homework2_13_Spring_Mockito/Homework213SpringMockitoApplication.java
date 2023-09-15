bedaa38d5d20f514d952be7c77b2a89c3b5aea5f
Alexsand74 <1974alexsand@gmail.com>
2023-09-15 22:24:02 +0300

    This is the initial version before writing the program!

---
+++ src/main/java/my/sky/pro/homework2_13_Spring_Mockito/Homework213SpringMockitoApplication.java
@@ -0,0 +1,13 @@
+package my.sky.pro.homework2_13_Spring_Mockito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Homework213SpringMockitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Homework213SpringMockitoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/my/sky/pro/homework2_13_Spring_Mockito/Homework213SpringMockitoApplicationTests.java
@@ -0,0 +1,13 @@
+package my.sky.pro.homework2_13_Spring_Mockito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Homework213SpringMockitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
